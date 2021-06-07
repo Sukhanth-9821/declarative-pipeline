5eb29544fbaa7b946e0526cb6dc1856615df5f4b
Sukhanth-9821 <sukhansukhanth@gmail.com>
2021-06-07 10:35:33 +0530

    Jenkinsfile-1 commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,16 @@
+
+
+pipeline{
+    agent any
+    stages{
+        stage("build"){
+            echo "This is a build stage"
+        }
+        stage("test"){
+            echo "This is a test stage"
+        }
+        stage("deploy"){
+            echo "THis is a deploy Stage"
+        }
+    }
+}
